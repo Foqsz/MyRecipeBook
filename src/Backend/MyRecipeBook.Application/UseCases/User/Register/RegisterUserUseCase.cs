7a70ff7f7b5923166949d323dda2663d5b5d69c9
Foqsz <contatovictorvinicius05@gmail.com>
2025-05-19 12:02:12 -0300

    feat: abordado uma maneira diferente de repositorios e interfaces, eu nunca fiz assim. Mas achei interessante a proteção de dados criando interfaces para cada metodo e também o padrão UnitOfWork para commits(saveChanges).

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -1,33 +1,47 @@
-﻿using MyRecipeBook.Application.Services.AutoMapper;
+﻿using AutoMapper;
+using MyRecipeBook.Application.Services.AutoMapper;
 using MyRecipeBook.Application.Services.Cryptografhy;
 using MyRecipeBook.Communication.Request;
 using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Repositories;
 using MyRecipeBook.Domain.Repositories.User;
 using MyRecipeBook.Exceptions.ExceptionsBase;
 
 namespace MyRecipeBook.Application.UseCases.User.Register;
 
-public class RegisterUserUseCase
+public class RegisterUserUseCase : IRegisterUserUseCase
 {
     private readonly IUserWriteOnlyRepository _userWriteOnlyRepository;
-    private readonly IUserReadOnlyRepository _userReadOnlyRepository;   
+    private readonly IUserReadOnlyRepository _userReadOnlyRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+    private readonly PasswordEncripter _passwordEncripter;
+
+    public RegisterUserUseCase(
+        IUserWriteOnlyRepository userWriteOnlyRepository,
+        IUserReadOnlyRepository userReadOnlyRepository,
+        IMapper mapper,
+        PasswordEncripter passwordEncripter,
+        IUnitOfWork unitOfWork)
+    {
+        _userWriteOnlyRepository = userWriteOnlyRepository;
+        _userReadOnlyRepository = userReadOnlyRepository;
+        _mapper = mapper;
+        _passwordEncripter = passwordEncripter;
+        _unitOfWork = unitOfWork;
+    }
 
     public async Task<ResponseRegisteredUserJson> Execute(RequestRegisterUserJson request)
     {
-        var criptografiaDeSenha = new PasswordEncripter();
-
-        var autoMapper = new AutoMapper.MapperConfiguration(options =>
-        {
-            options.AddProfile(new AutoMappingProfile());
-        }).CreateMapper();
-
         Validate(request);
           
-        var user = autoMapper.Map<Domain.Entities.User>(request);
+        var user = _mapper.Map<Domain.Entities.User>(request);
  
-        user.Password = criptografiaDeSenha.Encrypt(request.Password);
+        user.Password = _passwordEncripter.Encrypt(request.Password);
 
         await _userWriteOnlyRepository.Add(user);
+
+        await _unitOfWork.Commit();
 
         return new ResponseRegisteredUserJson
         {
