9d060ae4bbb3e192a262f108e249a208cb54fa20
Foqsz <contatovictorvinicius05@gmail.com>
2025-05-19 11:14:13 -0300

    feat: injection dependency

---
+++ src/Backend/MyRecipeBook.Infrastucture/DependencyInjectionExtension.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Domain.Repositories.User;
+using MyRecipeBook.Infrastucture.DataAccess;
+using MyRecipeBook.Infrastucture.DataAccess.Repositories;
+
+namespace MyRecipeBook.Infrastucture;
+public static class DependencyInjectionExtension
+{
+    public static void AddInfrastructure(this IServiceCollection services)
+    {
+        AddRepositories(services);
+        AddDbContext_SqlServer(services);
+    }
+
+    private static void AddDbContext_SqlServer(IServiceCollection services)
+    {
+        var connectionString = "Data Source=PC-VP;Initial Catalog=meulivrodereceitas;User ID=sa;Password=1967;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+
+        services.AddDbContext<MyRecipeBookDbContext>(dbContextOptions =>
+        {
+            dbContextOptions.UseSqlServer(connectionString);
+        });
+    }
+
+    private static void AddRepositories(IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
+        services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+    }
+}
