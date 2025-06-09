c031ef5669fffc48122c5c4c9e4f99e73676d85b
Foqsz <contatovictorvinicius05@gmail.com>
2025-06-09 12:21:39 -0300

    chorus: correção de code smeels do sonar

--- src/Backend/MyRecipeBook.Domain/Entities/User.cs
+++ src/Backend/MyRecipeBook.Domain/Entities/User.cs
@@ -2,9 +2,9 @@
 
 public class User : EntityBase
 {
-    public long Id { get; set; }
-    public bool Active { get; set; } = true;
-    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    //public long Id { get; set; }
+    //public bool Active { get; set; } = true;
+    //public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
