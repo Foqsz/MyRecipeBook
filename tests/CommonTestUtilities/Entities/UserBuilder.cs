003a6162920c0123f64ed0389b282d9159477e8a
Foqsz <contatovictorvinicius05@gmail.com>
2025-06-13 09:51:43 -0300

    feat: codigo refatorado pra evitar duplicaçao

--- tests/CommonTestUtilities/Entities/UserBuilder.cs
+++ tests/CommonTestUtilities/Entities/UserBuilder.cs
@@ -14,7 +14,7 @@ public class UserBuilder
         var user = new Faker<User>()
             .RuleFor(user => user.Id, () => 1)
             .RuleFor(user => user.Name, (f) => f.Person.FirstName)
-            .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Email))
+            .RuleFor(user => user.Email, (f, user) => f.Internet.Email(user.Name))
             .RuleFor(user => user.Password, (f) => passwordEncripter.Encrypt(password));
 
         return (user, password);
