cbebd25442586af83d3d539cab72d01446d8f970
Foqsz <contatovictorvinicius05@gmail.com>
2025-06-16 12:14:43 -0300

    feat: criando atributo personalizado pra autenticação. Adicionado também novas mensagens exceptions.

---
+++ src/Backend/MyRecipeBook.API/Attributes/AuthenticationUserAttribute.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyRecipeBook.API.Attributes;
+
+public class AuthenticationUserAttribute : TypeFilterAttribute
+{
+    public AuthenticationUserAttribute() : base(typeof(AuthenticatedUserFilter))
+    {
+    }
+}
