7bef0e50712a30f7299a8b77a20c89ff0f1374d1
Foqsz <contatovictorvinicius05@gmail.com>
2025-06-27 12:30:17 -0300

    feat: controller do tipo Post para a filtragem

--- src/Backend/MyRecipeBook.API/Controllers/RecipeController.cs
+++ src/Backend/MyRecipeBook.API/Controllers/RecipeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MyRecipeBook.API.Attributes;
 using MyRecipeBook.Application.UseCases.Recipe;
+using MyRecipeBook.Application.UseCases.Recipe.Filter;
 using MyRecipeBook.Communication.Request;
 using MyRecipeBook.Communication.Responses;
 
@@ -11,11 +12,25 @@ public class RecipeController : MyRecipeBookBaseController
 {
     [HttpPost]
     [ProducesResponseType(typeof(ResponseRegisteredRecipeJson), StatusCodes.Status201Created)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Register([FromServices] IRegisterRecipeUseCase useCase, [FromBody] RequestRecipeJson request)
     {
         var response = await useCase.Execute(request);
 
         return Created(string.Empty, response);
+    }
+
+    [HttpPost("filter")]
+    [ProducesResponseType(typeof(ResponseRecipesJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> Fiter([FromServices] IFilterRecipeUseCase useCase, [FromBody] RequestFilterRecipeJson request)
+    {
+        var response = await useCase.Execute(request);
+
+        if (response.Recipes.Any())
+            return Ok(response);
+
+        return NoContent();
+
     }
 }
