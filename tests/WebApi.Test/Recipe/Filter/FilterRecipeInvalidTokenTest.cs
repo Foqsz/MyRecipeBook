776bf6669a4b4fc12ef6c6e9acce46f13b9b7926
Foqsz <contatovictorvinicius05@gmail.com>
2025-06-28 12:38:13 -0300

    feat: testes de unidade para testar a filtragem das receitas

---
+++ tests/WebApi.Test/Recipe/Filter/FilterRecipeInvalidTokenTest.cs
@@ -0,0 +1,46 @@
+﻿using CommonTestUtilities.Requests;
+using CommonTestUtilities.Tokens;
+using Shouldly;
+using System.Net;
+
+namespace WebApi.Test.Recipe.Filter;
+public class FilterRecipeInvalidTokenTest : MyRecipeBookClassFixture
+{
+    private const string METHOD = "recipe/filter";
+
+    public FilterRecipeInvalidTokenTest(CustomWebApplicationFactory webApplication) : base(webApplication)
+    {
+    }
+
+    [Fact]
+    public async Task Error_Token_Invalid()
+    {
+        var request = RequestFilterRecipeJsonBuilder.Build();
+
+        var response = await DoPost(method: METHOD, request: request, token: "invalid_token");
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Error_Without_Token()
+    {
+        var request = RequestFilterRecipeJsonBuilder.Build();
+
+        var response = await DoPost(method: METHOD, request: request, token: null);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+
+    [Fact]
+    public async Task Error_Token_With_User_NotFound()
+    {
+        var request = RequestFilterRecipeJsonBuilder.Build();
+
+        var token = JwtTokenGeneratorBuild.Build().Generate(Guid.NewGuid());
+
+        var response = await DoPost(method: METHOD, request: request, token: token);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+    }
+}
