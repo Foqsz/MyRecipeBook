6a26b6004a758199ef545cde3f9c1dcd9eaff86f
Foqsz <contatovictorvinicius05@gmail.com>
2025-06-30 10:20:55 -0300

    feat: NotFoundException

---
+++ src/Shared/MyRecipeBook.Exceptions/ExceptionsBase/NotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace MyRecipeBook.Exceptions.ExceptionsBase;
+public class NotFoundException : MyRecipeBookException
+{
+    public NotFoundException(string message) : base(message) // recebo a mensagem de erro que será exibida ao usuário
+    {
+        
+    }
+}
