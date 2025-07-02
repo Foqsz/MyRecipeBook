d5533871b016963df984b109b7865fd64fe28d2c
Foqsz <contatovictorvinicius05@gmail.com>
2025-07-02 12:13:52 -0300

    feat: testes de unidade para o fluxo de deletar receita

--- tests/WebApi.Test/MyRecipeBookClassFixture.cs
+++ tests/WebApi.Test/MyRecipeBookClassFixture.cs
@@ -36,6 +36,14 @@ public class MyRecipeBookClassFixture : IClassFixture<CustomWebApplicationFactor
         return await _httpClient.PutAsJsonAsync(method, request);
     }
 
+    protected async Task<HttpResponseMessage> DoDelete(string method, string token, string culture = "pt-BR")
+    {
+        ChangeRequestCulture(culture);
+        AuthorizeRequest(token);
+
+        return await _httpClient.DeleteAsync(method);
+    }
+
     private void ChangeRequestCulture(string culture)
     {
         if (_httpClient.DefaultRequestHeaders.Contains("Accept-Language"))
---
+++ tests/WebApi.Test/Recipe/Delete/DeleteRecipeTest.cs
@@ -0,0 +1,62 @@
+﻿using CommonTestUtilities.IdEncryption;
+using CommonTestUtilities.Tokens;
+using MyRecipeBook.Exceptions;
+using Shouldly;
+using System.Globalization;
+using System.Net;
+using System.Text.Json;
+using WebApi.Test.InlineData;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace WebApi.Test.Recipe.Delete;
+public class DeleteRecipeTest : MyRecipeBookClassFixture
+{
+    private const string METHOD = "recipe";
+
+    private readonly Guid _userIdentifier;
+    private readonly string _recipeId;
+
+    public DeleteRecipeTest(CustomWebApplicationFactory factory) : base(factory)
+    {
+        _userIdentifier = factory.GetUserIdentifier();
+        _recipeId = factory.GetRecipeId();
+    }
+
+    [Fact]
+    public async Task Success()
+    {
+        var token = JwtTokenGeneratorBuild.Build().Generate(_userIdentifier);   
+
+        var response = await DoDelete($"{METHOD}/{_recipeId}", token: token);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+
+        response = await DoGet($"{METHOD}/{_recipeId}", token: token);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+    }
+
+    [Theory]
+    [ClassData(typeof(CultureInlineDataTest))]
+    public async Task Error_Recipe_NotFound(string culture)
+    {
+        var token = JwtTokenGeneratorBuild.Build().Generate(_userIdentifier);
+
+        var id = IdEncripterBuilder.Build().Encode(100);
+
+        var response = await DoDelete($"{METHOD}/{id}", token: token, culture: culture);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+
+        await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+        var responseData = await JsonDocument.ParseAsync(responseBody);
+
+        var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+        var expectedMessage = ResourceMessagesException.ResourceManager.GetString("RECIPE_NOT_FOUND", new CultureInfo(culture));
+
+        errors.Count().ShouldBe(1);
+        errors.Any(c => c.GetString() == expectedMessage).ShouldBeTrue();
+    }
+}
