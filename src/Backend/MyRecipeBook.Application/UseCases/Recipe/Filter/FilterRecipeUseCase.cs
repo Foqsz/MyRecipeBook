bce0292a0935d6d4d2af0b0a838b9b42c64cfdc6
Foqsz <contatovictorvinicius05@gmail.com>
2025-07-14 11:51:21 -0300

    feat: recuperando lista de receitas com imagens

--- src/Backend/MyRecipeBook.Application/UseCases/Recipe/Filter/FilterRecipeUseCase.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/Recipe/Filter/FilterRecipeUseCase.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
+using MyRecipeBook.Application.Extensions;
 using MyRecipeBook.Communication.Responses;
 using MyRecipeBook.Domain.Extensions;
 using MyRecipeBook.Domain.Repositories.Recipe;
 using MyRecipeBook.Domain.Services.LoggedUser;
+using MyRecipeBook.Domain.Services.Storage;
 using MyRecipeBook.Exceptions.ExceptionsBase;
 
 namespace MyRecipeBook.Application.UseCases.Recipe.Filter;
@@ -11,12 +13,14 @@ public class FilterRecipeUseCase : IFilterRecipeUseCase
     private readonly IMapper _mapper;
     private readonly ILoggedUser _loggedUser;
     private readonly IRecipeReadOnlyRepository _repository;
+    private readonly IBlobStorageService _blobStorageService;
 
-    public FilterRecipeUseCase(IMapper mapper, ILoggedUser loggedUser, IRecipeReadOnlyRepository repository)
+    public FilterRecipeUseCase(IMapper mapper, ILoggedUser loggedUser, IRecipeReadOnlyRepository repository, IBlobStorageService blobStorageService)
     {
         _mapper = mapper;
         _loggedUser = loggedUser;
         _repository = repository;
+        _blobStorageService = blobStorageService;
     }
 
     public async Task<ResponseRecipesJson> Execute(RequestFilterRecipeJson request)
@@ -37,7 +41,7 @@ public class FilterRecipeUseCase : IFilterRecipeUseCase
 
         return new ResponseRecipesJson()
         {
-            Recipes = _mapper.Map<List<ResponseShortRecipeJson>>(recipes)    
+            Recipes = await recipes.MapToShortRecipeJson(loggedUser, _blobStorageService, _mapper)
         };
     }
 
